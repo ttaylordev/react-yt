15d7ef51ef720020475fe0dc1008a7c54527dac8
Tyson <mellowt@gmail.com>
2016-07-18 12:26:44 -0600

    started with a fresh project, deleted src folder, and re-created

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Create a new component. This component shoudl produce some HTML
 
-import App from './components/app';
-import reducers from './reducers';
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and place it on the DOM.
